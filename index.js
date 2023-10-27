7168c44cd5e3f2cbb7247f63107aadccb3d7f4ee
tristandevs <113070063+tristandevs@users.noreply.github.com>
2023-10-27 09:45:48 -0400

    .

---
+++ index.js
@@ -0,0 +1,11 @@
+const Fastify = require('fastify');
+const server = Fastify();
+
+server.register(require('@fastify/http-proxy'), {
+  upstream: 'https://shuttleproxy.com',
+  prefix: '/', 
+  http2: false,
+});
+
+server.listen({host: "0.0.0.0", port: 3000 });
+

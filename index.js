cebb3db92a38b0d5d58d94548e01a4d272122683
FireStreaker2 <michaelccc2019@outlook.com>
2023-11-05 16:25:31 -0800

    add required files

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const Fastify = require('fastify');
+const Fastify = require("fastify");
 const server = Fastify();
 
-server.register(require('@fastify/http-proxy'), {
-  upstream: 'https://shuttleproxy.com',
-  prefix: '/', 
-  http2: false,
+server.register(require("@fastify/http-proxy"), {
+	upstream: "https://shuttleproxy.com",
+	prefix: "/",
+	http2: false,
 });
 
-server.listen({host: "0.0.0.0", port: 3000 });
-
+server.listen({ host: "0.0.0.0", port: 3000 }, () => {
+	console.log("shuttle-lite is listening on port 3000");
+});

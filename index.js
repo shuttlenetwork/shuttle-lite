4571e9b6987fcb5ca4604161750401fa1997bf3c
Ruslan Osipov <rusya13@gmail.com>
2025-08-05 15:52:51 +1000

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Fastify = require("fastify");
 const server = Fastify();
 
 server.register(require("@fastify/http-proxy"), {
-	upstream: "https://shuttleproxy.com",
+	upstream: "https://pr.shuttleproxy.com",
 	prefix: "/",
 	http2: false,
 });
